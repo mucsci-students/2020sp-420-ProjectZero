553e135e46ffcafaa556ad53f6454830ad5938c2
Ryan Malesic <ryanmalesic@icloud.com>
2020-01-29 18:09:14 -0500

    Initialize structure with gradle and subprojects

---
+++ core/src/main/java/projectzero/Main.java
@@ -0,0 +1,7 @@
+package projectzero;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ core/src/test/java/projectzero/TestMain.java
@@ -0,0 +1,12 @@
+package projectzero;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(2, 2);
+    }
+}
