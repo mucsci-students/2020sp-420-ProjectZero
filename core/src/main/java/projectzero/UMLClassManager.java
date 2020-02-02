14730193f9d7c195c9f1364e8135f3f186a91cca
Dylan Dewald <dylan.dewald94@gmail.com>
2020-02-01 22:20:38 -0500

    First design of UMLClassManager and UMLClass

---
+++ core/src/main/java/projectzero/UMLClassManager.java
@@ -0,0 +1,58 @@
+package projectzero;
+
+import java.util.LinkedList;
+
+public class UMLClassManager {
+    private LinkedList<UMLClass> classList;
+
+    public UMLClassManager() {
+        classList = new LinkedList<UMLClass>();
+    }
+
+    public boolean addClass(UMLClass newClass) {
+        for (UMLClass classObject : classList) {
+            //Found a duplicate in list already.
+            if (newClass.getName().equals(classObject.getName())) {
+                return false;
+            }
+        }
+        classList.add(newClass);
+        return true;
+
+    }
+
+    public boolean deleteClass(UMLClass classToBeDeleted) {
+        boolean deleatedClass = false;
+        if (!classList.isEmpty() || classToBeDeleted != null) {
+            UMLClass tempClass = null;
+
+            for (UMLClass classObject : classList) {
+                if (classObject.getName().equals(classToBeDeleted.getName())) {
+                    tempClass = classObject;
+                    deleatedClass = true;
+                    break;
+                }
+            }
+            if (deleatedClass == true) {
+                classList.remove(tempClass);
+                return deleatedClass;
+            } else {
+                return deleatedClass;
+            }
+        }
+        return deleatedClass;
+        //What happens if the classObject is not found?
+    }
+
+    public void saveWork(String path) {
+
+    }
+
+    public void loadWork(String path) {
+
+    }
+
+    public LinkedList<UMLClass> getClassList() {
+        return classList;
+    }
+}
