fbd52e978f876f99218e9f5f50c65201ac96f287
mslint <maryannlint@gmail.com>
2020-02-06 19:02:19 -0500

    Reformat Application Class

--- cli-app/src/main/java/projectzero/Application.java
+++ cli-app/src/main/java/projectzero/Application.java
@@ -5,24 +5,37 @@ import java.util.Scanner;
 public class Application {
     private UMLClassManager MainManager;
     private Scanner scan = new Scanner(System.in);
+
     public Application() {
         MainManager = new UMLClassManager();
-
-
     }
 
     public void run() {
         initMenuPrompt();
-        int input = scan.nextInt();
-        if (input == 2) {
-            System.out.println("Enter the file path: ");
-            //handle bad case later
-            MainManager.loadWork(scan.nextLine());
-        }
+        MainMenu();
+    }
 
-
+    private void initMenuPrompt() {
+        int input;
         do {
-            MainMenu();
+            System.out.println("Create or load a new file?");
+            System.out.println("1: create a new project\n2: load a previous project");
+            input = scan.nextInt();
+            if (input == 2) {
+                System.out.println("Enter the file path: ");
+                //handle bad case later
+                MainManager.load(scan.nextLine());
+            } else if (input != 1) {
+                System.out.println("Error: invalid option");
+            }
+        } while (input != 1 || input != 2);
+    }
+
+    private void MainMenu() {
+        int input;
+        do {
+            System.out.println(" 1. Display Class List \n 2. Add Class \n 3. Delete Class \n 4. Save Work \n 5. Quit");
+            System.out.println("Enter number for option: ");
             input = scan.nextInt();
             String className;
             switch (input) {
@@ -44,24 +57,12 @@ public class Application {
                 case 4:
                     System.out.println("Enter file path to save to: ");
                     String filePath = scan.nextLine();
-                     if (MainManager.saveWork(filePath))
-                         System.out.println( "file was saved to" + filePath);
-                     else
-                         System.out.println( "Error: file was not saved to" + filePath);
-                     break;
+                    if (MainManager.save(filePath))
+                        System.out.println("file was saved to" + filePath);
+                    else
+                        System.out.println("Error: file was not saved to" + filePath);
+                    break;
             }
         } while (input != 5);
-    }
-
-    private void initMenuPrompt(){
-        System.out.println("Create or load a new file?");
-        System.out.println("1: create a new project\n2: load a previous project");
-    }
-
-
-    private void MainMenu() {
-        System.out.println(" 1. Display Class List \n 2. Add Class \n 3. Delete Class \n 4. Save Work \n 5. Quit");
-        System.out.println("Enter number for option: ");
-
     }
 }
