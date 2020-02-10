a5410130744177029fb8f03b4c114b6f97b89c31
Emmanouil Richman <erichman966@gmail.com>
2020-02-10 18:09:12 -0500

    Clean up code, format menus and fix bugs

--- cli-app/src/main/java/projectzero/Application.java
+++ cli-app/src/main/java/projectzero/Application.java
@@ -1,4 +1,6 @@
 package projectzero;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
@@ -17,56 +19,91 @@ public class Application {
     private void initMenuPrompt() {
         String input = "";
         boolean validInput = true;
+        boolean inInitMenu = true;
         int[] validInputs = {1,2};
         do {
             System.out.println("Create or load a new file?");
             System.out.println("1: create a new project\n2: load a previous project");
             input = scan.next();
+            System.out.println();
             validInput = Validation.isValidMenuInput(validInputs, input);
             if (validInput) {
+                if(Integer.parseInt(input) == 1){
+                    inInitMenu = false;
+                }
                 //does not collect file path name, skips to main menu
                 if (Integer.parseInt(input) == 2) {
-                    System.out.println("Enter the file path: ");
-                    MainManager.load(scan.nextLine());
+                    System.out.println("Enter the file path followed by the desired name: ");
+                    if(MainManager.load(scan.next())){
+                        System.out.println("\nFile retrieved successfully\n");
+                        inInitMenu = false;
+                    }
+                    else{
+                        System.out.println("\nFailed to load file\n");
+                        inInitMenu = true;
+                    }
+
                 }
             }
-        } while (!validInput);
+        } while (inInitMenu);
 
     }
 
     private void MainMenu() {
-        int input;
+        String input;
+        boolean inMainMenu = true;
+        boolean validInput = true;
+        int[] validInputs = {1,2,3,4,5};
         do {
             System.out.println(" 1. Display Class List \n 2. Add Class \n 3. Delete Class \n 4. Save Work \n 5. Quit");
-            System.out.println("Enter number for option: ");
-            input = scan.nextInt();
-            String className;
-            switch (input) {
-                case 1:
-                    System.out.println(MainManager.getClassList());
-                    break;
-                case 2:
-                    System.out.println("Enter the class name: ");
-                    className = scan.nextLine();
-                    MainManager.addClass(new UMLClass(className));
-                    System.out.println(className + "was added");
-                    break;
-                case 3:
-                    System.out.println("Enter the class name: ");
-                    className = scan.nextLine();
-                    MainManager.deleteClass(new UMLClass(className));
-                    System.out.println(className + "was deleted");
-                    break;
-                case 4:
-                    System.out.println("Enter file path to save to: ");
-                    String filePath = scan.nextLine();
-                    if (MainManager.save(filePath))
-                        System.out.println("file was saved to" + filePath);
-                    else
-                        System.out.println("file was not saved to" + filePath);
-                    break;
-                default :
+            System.out.println("Enter number for option:");
+            input = scan.next();
+            validInput = Validation.isValidMenuInput(validInputs, input);
+            if (validInput) {
+                String className;
+                switch (Integer.parseInt(input)) {
+                    case 1:
+                        printList();
+                        break;
+                    case 2:
+                        System.out.println("Enter the class name: ");
+                        className = scan.next();
+                        MainManager.addClass(new UMLClass(className));
+                        System.out.println(className + " was added\n");
+                        break;
+                    case 3:
+                        System.out.println("Enter the class name: ");
+                        className = scan.next();
+                        if(MainManager.deleteClass(new UMLClass(className))){
+                            System.out.println(className + " was deleted\n");
+                        }
+                        else{
+                            System.out.println("Class not found\n");
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Enter file path to save to: ");
+                        String filePath = scan.next();
+                        if (MainManager.save(filePath))
+                            System.out.println("File was saved to" + filePath + "\n");
+                        else
+                            System.out.println("File was not saved to" + filePath + "\n");
+                        break;
+                    case 5:
+                        inMainMenu = false;
+                    default:
+                }
             }
-        } while (input != 5);
+        } while (inMainMenu);
+    }
+
+    private void printList(){
+        List<UMLClass> tempList = MainManager.getClassList();
+        System.out.println("\nCurrent classes:");
+        System.out.print("[ ");
+        for(UMLClass tempClass: tempList){
+            System.out.print(tempClass.getName() + " ");
+        }
+        System.out.println("]\n");
     }
 }
