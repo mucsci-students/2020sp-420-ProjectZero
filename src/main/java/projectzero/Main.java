b6ad465e703695b61995e2022bbd4286da883fc4
Ryan Malesic <ryanmalesic@icloud.com>
2020-02-19 19:34:23 -0500

    Fix bug where cli arguments didn't work

--- src/main/java/projectzero/Main.java
+++ src/main/java/projectzero/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         String version = "1";
 
-        if (args.length > 1) {
+        if (args.length > 0) {
             version = args[0];
         }
 
-        if (args.equals("0")) {
+        if (version.equals("0")) {
             CliApplication cliApplication = new CliApplication();
             cliApplication.run();
         } else {
