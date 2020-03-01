987b01b19914a84c0dc429c106b4e8f95ca9f140
mslint <maryannlint@gmail.com>
2020-02-29 21:09:23 -0500

    Add ClassNode Class and clickable function

--- src/main/java/projectzero/fx/GraphicsPane.java
+++ src/main/java/projectzero/fx/GraphicsPane.java
@@ -1,9 +1,17 @@
 package projectzero.fx;
 
 import javafx.scene.layout.StackPane;
+import projectzero.core.Field;
+import projectzero.core.Method;
+import projectzero.core.UmlClass;
 
 public class GraphicsPane extends StackPane{
     public GraphicsPane(){
-
+        this.setPrefSize(200,200);
+        UmlClass testClass = new UmlClass("testName");
+        testClass.addField(new Field("fieldName"));
+        testClass.addMethod(new Method("methodName"));
+        ClassNode testClassNode = new ClassNode(testClass);
+        this.getChildren().add(testClassNode);
     }
 }
