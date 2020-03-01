d1760bb9fb6435ba704ef5783d71da5aa8c547e3
Dylan Dewald <dylan.dewald94@gmail.com>
2020-02-29 19:03:00 -0500

    Alteration to ClassScreen to make it usuable in two different scenarios.

--- src/main/java/projectzero/fx/ClassScreen.java
+++ src/main/java/projectzero/fx/ClassScreen.java
@@ -1,5 +1,6 @@
 package projectzero.fx;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -8,39 +9,44 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ClassScreen extends Stage {
-    private TextArea methodDisplay, fieldDisplay, relationshipDisplay;
+    private ListView<String> methodDisplay, fieldDisplay, relationshipDisplay;
+    private ObservableList<String> methods, fields,relationships;
     private final double WIDTH=280,HEIGHT=500;
     private final String TITLE = "Class Mod Screen";
+
     public ClassScreen(){
         VBox root = new VBox();
-        methodDisplay = new TextArea();
-        fieldDisplay = new TextArea();
-        relationshipDisplay = new TextArea();
+
+        setupListViews();
 
         HBox classNameInfo = new HBox();
         classNameInfo.getChildren().addAll(new Label("Class name: "), new TextField());
 
         HBox methodInfo = new HBox();
         //Fix later to not accept whitespace stuff/error checking. Also add to classManager.
-        Button methodAddButton = setupAddButton(methodInfo,methodDisplay);
+        Button methodAddButton = setupAddButton(methodInfo,methods);
         setupInfoField(methodInfo,"Method name: ", methodAddButton);
 
         HBox fieldInfo = new HBox();
         //Fix later to not accept whitespace stuff/error checking. Also add to classManager.
-        Button fieldAddButton = setupAddButton(fieldInfo,fieldDisplay);
+        Button fieldAddButton = setupAddButton(fieldInfo,fields);
         setupInfoField(fieldInfo,"Field name: ", fieldAddButton);
 
         HBox relationshipInfo = new HBox();
         ComboBox<String> cBox = new ComboBox<String>();
         cBox.getItems().addAll("Dude", "Pizza Guy", "Another dude");
-        Button relationshipButton = setupAddButton(relationshipInfo,relationshipDisplay, cBox);
+        Button relationshipButton = setupAddButton(relationshipInfo,relationships, cBox);
         setupInfoField(relationshipInfo,"Relationships: ",cBox, relationshipButton);
 
         VBox displayInfo = new VBox();
 
+        HBox hMethods = setupListViewWithMinus(methodDisplay,methods);
+        HBox hFields = setupListViewWithMinus(fieldDisplay,fields);
+        HBox hRelationships = setupListViewWithMinus(relationshipDisplay,relationships);
+
         displayInfo.getChildren().addAll(new Label("Methods:"),
-                methodDisplay,new Label("Fields:"),fieldDisplay
-                ,new Label("Relationships:"),relationshipDisplay);
+                hMethods,new Label("Fields:"),hFields
+                ,new Label("Relationships:"),hRelationships);
 
         HBox buttonChoices = new HBox();
         buttonChoices.getChildren().addAll(new Button("Apply"), new Button("Cancel"));
@@ -51,24 +57,20 @@ public class ClassScreen extends Stage {
         this.setTitle(TITLE);
     }
 
-    private Button setupAddButton(HBox container, TextArea display){
+    private Button setupAddButton(HBox container, ObservableList<String> displayList){
         Button addButton = new Button("+");
         //Fix later to not accept whitespace stuff/error checking. Also add to classManager.
         addButton.setOnAction(e-> {
-            display.appendText(((TextField)container.getChildren().get(1)).getText()
-                    + "\n");
+            displayList.add(((TextField)container.getChildren().get(1)).getText());
             ((TextField)container.getChildren().get(1)).setText("");
         });
         return addButton;
     }
 
-    private Button setupAddButton(HBox container, TextArea display, ComboBox cBox){
+    private Button setupAddButton(HBox container, ObservableList<String> displayList, ComboBox cBox){
         Button addButton = new Button("+");
         //Fix later to not accept whitespace stuff/error checking. Also add to classManager.
-        addButton.setOnAction(e-> {
-            display.appendText(cBox.getSelectionModel().getSelectedItem()
-                    + "\n");
-        });
+        addButton.setOnAction(e-> displayList.add((String)cBox.getSelectionModel().getSelectedItem()));
         return addButton;
     }
 
@@ -78,5 +80,27 @@ public class ClassScreen extends Stage {
     private void setupInfoField(HBox container, String labelName, ComboBox<String> cBox, Button button){
         container.getChildren().addAll(new Label(labelName),cBox,button);
 
+    }
+
+    private void setupListViews(){
+        methodDisplay = new ListView<String>();
+        fieldDisplay = new ListView<String>();
+        relationshipDisplay = new ListView<String>();
+
+        methods = FXCollections.observableArrayList();
+        fields = FXCollections.observableArrayList();
+        relationships = FXCollections.observableArrayList();
+
+        methodDisplay.setItems(methods);
+        fieldDisplay.setItems(fields);
+        relationshipDisplay.setItems(relationships);
+    }
+
+    private HBox setupListViewWithMinus(ListView<String> display,ObservableList<String> displayList){
+        HBox box = new HBox();
+        Button minusButton = new Button("-");
+        minusButton.setOnAction(e -> displayList.remove(display.getSelectionModel().getSelectedItem()));
+        box.getChildren().addAll(display,minusButton);
+        return box;
     }
 }
