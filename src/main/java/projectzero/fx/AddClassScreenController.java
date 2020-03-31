d46bf33a9938053b763d0ce9c3cfff7c171d721b
Dylan Dewald <dylan.dewald94@gmail.com>
2020-03-31 03:36:08 -0400

    Add class screen update to meet type requirements.

--- src/main/java/projectzero/fx/AddClassScreenController.java
+++ src/main/java/projectzero/fx/AddClassScreenController.java
@@ -13,27 +13,21 @@ public class AddClassScreenController  extends ClassScreenController{
     @Override
     public void applyUMLClass(ActionEvent event) {
         try {
-            UmlClass umlClass = new UmlClass(textBoxClass.getText());
-            for(String method: methods)
-                umlClass.addMethod(new Method(method));
-            for(String field: fields)
-                umlClass.addField((new Field(field)));
-            //Get rid of this code later. Because the relationships need to be
-            //existing classes.
-            for(String relationship: relationships)
-                umlClass.addRelationship(new Relationship(mainManager.getUmlClass(relationship)));
+            UmlClass umlClass = new UmlClass(textBoxClassName.getText());
+            for(Method method: methods)
+                umlClass.addMethod(method);
+            for(Field field: fields)
+                umlClass.addField(field);
+            for(Relationship relationship: relationships)
+                umlClass.addRelationship(relationship);
+
             if(mainManager.addUmlClass(umlClass) == null)
                 updateComboBoxRelationships(umlClass);
-            textBoxClass.setStyle("-fx-text-box-border: #9A9A9A; -fx-focus-color: darkslateblue;");
+            textBoxClassName.setStyle("-fx-text-box-border: #9A9A9A; -fx-focus-color: darkslateblue;");
             fieldDisplay.setStyle("-fx-border-color: #9A9A9A; -fx-focus-color: darkslateblue;");
             resetData();
         } catch (InvalidNameException e) {
-            textBoxClass.setStyle(" -fx-text-box-border: red ; -fx-focus-color: red ;");
+            textBoxClassName.setStyle(" -fx-text-box-border: red ; -fx-focus-color: red ;");
         }
-   /*     catch(DuplicateAttributeException e){
-            fieldDisplay.setStyle(" -fx-border-color: red ; -fx-focus-color: red ;");
-        }*/
-
-
     }
 }
--- src/main/java/projectzero/fx/ClassScreenController.java
+++ src/main/java/projectzero/fx/ClassScreenController.java
@@ -1,51 +1,90 @@
 package projectzero.fx;
 
+import com.sun.javafx.scene.control.skin.ComboBoxListViewSkin;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import projectzero.core.*;
-import projectzero.core.exceptions.DuplicateAttributeException;
 import projectzero.core.exceptions.InvalidNameException;
-
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public abstract class ClassScreenController implements Initializable {
-    protected ObservableList<String> methods, fields, relationships;
+    protected ObservableList<Method> methods;
+    protected ObservableList<Field> fields;
+    protected ObservableList<Relationship> relationships;
     protected UmlClassManager mainManager;
     @FXML
-    protected TextField textBoxClass;
+    protected TextField textBoxClassName;
     @FXML
-    protected TextField textBoxMethods;
+    protected TextField textBoxMethodName;
     @FXML
-    protected TextField textBoxFields;
+    protected TextField textBoxMethodReturnType;
     @FXML
-    protected ComboBox<String> comboRelationships;
+    protected TextField textBoxParamType;
     @FXML
-    protected ListView<String> methodDisplay, fieldDisplay, relationshipDisplay;
+    protected ComboBox<String> comboMethodParameterTypes;
+    @FXML
+    protected TextField textBoxFieldName;
+    @FXML
+    protected TextField textBoxFieldType;
+    @FXML
+    protected ComboBox<String> comboRelationshipToName;
+    @FXML
+    protected ComboBox<Relationship.Type> comboRelationshipType;
+    @FXML
+    protected ListView<Method>  methodDisplay;
+    @FXML
+    protected ListView<Field>   fieldDisplay;
+    @FXML
+    protected ListView<Relationship>   relationshipDisplay;
 
     public void addMethod(ActionEvent e) {
-        methods.add(textBoxMethods.getText());
-        textBoxMethods.setText("");
+        try {
+            methods.add(new Method.Builder().withName(textBoxMethodName.getText())
+                            .withType(textBoxMethodReturnType.getText())
+                            .withParameterTypes(new ArrayList<String>(comboMethodParameterTypes.getItems()))
+                            .build());
+            textBoxMethodName.setStyle("-fx-text-box-border: #9A9A9A; -fx-focus-color: darkslateblue;");
+            textBoxMethodName.setText("");
+            textBoxMethodReturnType.setText("");
+            comboMethodParameterTypes.getItems().removeAll(comboMethodParameterTypes.getItems());
+        } catch (InvalidNameException ex) {
+            textBoxMethodName.setStyle("-fx-text-box-border: red ; -fx-focus-color: red ;");
+        }
     }
 
     public void addField(ActionEvent e) {
-        fields.add(textBoxFields.getText());
-        textBoxFields.setText("");
+        try {
+            fields.add(new Field.Builder().withName(textBoxFieldName.getText())
+                            .withType(textBoxFieldType.getText())
+                            .build());
+            textBoxFieldName.setStyle("-fx-text-box-border: #9A9A9A; -fx-focus-color: darkslateblue;");
+            textBoxFieldName.setText("");
+            textBoxFieldType.setText("");
+        } catch (InvalidNameException ex) {
+            textBoxFieldName.setStyle("-fx-text-box-border: red ; -fx-focus-color: red ;");
+        }
+
     }
 
     public void addRelationship(ActionEvent e) {
-        relationships.add(comboRelationships.getSelectionModel().getSelectedItem());
+        relationships.add(new Relationship.Builder()
+                            .withTo(mainManager.getUmlClass(comboRelationshipToName.getSelectionModel().getSelectedItem()))
+                            .withType(comboRelationshipType.getSelectionModel().getSelectedItem()).build());
+        comboRelationshipToName.getSelectionModel().clearSelection();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setupListViews();
+        fillRelationships();
     }
 
     public void minusMethod(ActionEvent e) {
@@ -59,19 +98,22 @@ public abstract class ClassScreenController implements Initializable {
     public void minusRelationship(ActionEvent e) {
         relationships.remove(relationshipDisplay.getSelectionModel().getSelectedItem());
     }
+    public void addParamType(ActionEvent e){
+        comboMethodParameterTypes.getItems().add(textBoxParamType.getText());
+        textBoxParamType.setText("");
+    }
     public abstract void applyUMLClass(ActionEvent event);
 
     public void setUMLClassManager(UmlClassManager mainManager){
         this.mainManager = mainManager;
-        fillRelationships();
     }
 
     protected void fillRelationships(){
-        mainManager.listUmlClasses().forEach(umlClass -> comboRelationships.getItems().add(umlClass.getName()));
+        mainManager.listUmlClasses().forEach(umlClass -> comboRelationshipToName.getItems().add(umlClass.getName()));
     }
 
     protected void updateComboBoxRelationships(UmlClass umlClass){
-        comboRelationships.getItems().add(umlClass.getName());
+        comboRelationshipToName.getItems().add(umlClass.getName());
     }
 
     private void setupListViews() {
@@ -82,13 +124,60 @@ public abstract class ClassScreenController implements Initializable {
         methodDisplay.setItems(methods);
         fieldDisplay.setItems(fields);
         relationshipDisplay.setItems(relationships);
+
+        comboRelationshipType.getItems().addAll(Relationship.Type.values());
+
+        setupMethodParamsCombo();
     }
 
+    private void setupMethodParamsCombo(){
+        comboMethodParameterTypes.setCellFactory(lv ->
+            new ListCell<String>(){
+                HBox comboParamsLayout;
+                {
+                    Label paramLabel = new Label();
+                    paramLabel.textProperty().bind(itemProperty());
+                    paramLabel.setMaxWidth(Double.POSITIVE_INFINITY);
+                    paramLabel.setOnMouseClicked(event -> comboMethodParameterTypes.hide());
+
+                    Button buttonParamTypeMinus = new Button("-");
+                    buttonParamTypeMinus.setOnAction(event -> {
+                        String paramType = getItem();
+                        comboMethodParameterTypes.getItems().remove(paramType);
+                    });
+                    comboParamsLayout = new HBox(paramLabel,buttonParamTypeMinus);
+                    comboParamsLayout.setHgrow(paramLabel, Priority.ALWAYS);
+                    setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                }
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                    } else {
+                        setGraphic(comboParamsLayout);
+                    }
+                }
+            }
+        );
+        comboMethodParameterTypes.setSkin(new ComboBoxListViewSkin<String>(comboMethodParameterTypes) {
+            @Override
+            protected boolean isHideOnClickEnabled() {
+                return false;
+            }
+        });
+    }
     protected void resetData(){
-        textBoxClass.setText("");
-        textBoxMethods.setText("");
-        textBoxFields.setText("");
-        comboRelationships.getSelectionModel().clearSelection();
+        textBoxClassName.setText("");
+        textBoxMethodName.setText("");
+        textBoxMethodReturnType.setText("");
+        textBoxParamType.setText("");
+        textBoxFieldName.setText("");
+        textBoxFieldType.setText("");
+        comboMethodParameterTypes.getItems().removeAll(comboMethodParameterTypes.getItems());
+        comboMethodParameterTypes.getSelectionModel().clearSelection();
+        comboRelationshipToName.getSelectionModel().clearSelection();
+        comboRelationshipType.getSelectionModel().clearSelection();
         methods.removeAll(methods);
         fields.removeAll(fields);
         relationships.removeAll(relationships);
