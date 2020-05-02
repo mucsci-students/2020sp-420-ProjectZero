310470a78e3c0d1da5b9859563b06eb316a2243f
Fallen-Dylan <dedewald@millersville.edu>
2020-05-02 00:27:24 -0400

    A couple tests written for classScreenViewTest

--- src/test/java/projectzero/fx/ClassScreenViewTest.java
+++ src/test/java/projectzero/fx/ClassScreenViewTest.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
@@ -15,7 +16,6 @@ import projectzero.core.Field;
 import projectzero.core.Method;
 import projectzero.core.UmlClassManager;
 import projectzero.fx.controllers.ClassScreenViewController;
-
 import java.io.IOException;
 
 @ExtendWith(ApplicationExtension.class)
@@ -23,9 +23,10 @@ public class ClassScreenViewTest {
     ClassScreenViewController classScreenViewController;
     GridPane gridPane;
     UmlClassManager umlClassManager;
+    Scene testScene;
 
     @Start
-    private void start(Stage stage) throws IOException {
+    private void start(Stage stage) throws IOException{
         umlClassManager = new UmlClassManager();
         classScreenViewController = new ClassScreenViewController(null, umlClassManager);
 
@@ -34,7 +35,8 @@ public class ClassScreenViewTest {
 
         gridPane = loader.load();
 
-        stage.setScene(new Scene(gridPane));
+        testScene = new Scene(gridPane);
+        stage.setScene(testScene);
         stage.show();
     }
 
@@ -54,12 +56,88 @@ public class ClassScreenViewTest {
     }
 
     @Test
+    void testEmptyClassBox(FxRobot robot){
+        robot.clickOn("#applyButton");
+        TextField textBoxClassName = (TextField) testScene.lookup("#textBoxClassName");
+        Assertions.assertThat(textBoxClassName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
+        Assertions.assertThat(umlClassManager.listUmlClasses().size()).isEqualTo(0);
+
+    }
+
+    @Test
+    void testEmptyFieldBox(FxRobot robot){
+        robot.clickOn("#textBoxClassName");
+        robot.write("Class");
+        robot.clickOn("#addFieldButton");
+
+        TextField textBoxFieldName = (TextField) testScene.lookup("#textBoxFieldName");
+        Assertions.assertThat(textBoxFieldName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
+        robot.clickOn("#applyButton");
+        Assertions.assertThat(umlClassManager.listUmlClasses().size()).isEqualTo(1);
+        Assertions.assertThat(umlClassManager.getUmlClass("Class").getFields().size()).isEqualTo(0);
+    }
+
+    @Test
+    void testEmptyMethodBox(FxRobot robot){
+        robot.clickOn("#textBoxClassName");
+        robot.write("Class");
+        robot.clickOn("#addMethodButton");
+
+        TextField textBoxMethodName = (TextField) testScene.lookup("#textBoxMethodName");
+        Assertions.assertThat(textBoxMethodName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
+        robot.clickOn("#applyButton");
+
+        Assertions.assertThat(umlClassManager.listUmlClasses().size()).isEqualTo(1);
+        Assertions.assertThat(umlClassManager.getUmlClass("Class").getMethods().size()).isEqualTo(0);
+    }
+
+    @Test
     void testAddBadClassName(FxRobot robot) {
         robot.clickOn("#textBoxClassName");
         robot.write("@Class");
         robot.clickOn("#applyButton");
+        TextField textBoxClassName = (TextField) testScene.lookup("#textBoxClassName");
 
+        Assertions.assertThat(textBoxClassName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
         Assertions.assertThat(umlClassManager.listUmlClasses().size()).isEqualTo(0);
+    }
+
+    @Test
+    void testAddBadFieldName(FxRobot robot) {
+        robot.clickOn("#textBoxFieldName");
+        robot.write("@Field");
+        robot.clickOn("#addFieldButton");
+        TextField textBoxFieldName = (TextField) testScene.lookup("#textBoxFieldName");
+
+        Assertions.assertThat(textBoxFieldName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
+    }
+
+    @Test
+    void testAddBadFieldNameThenGoodFieldName(FxRobot robot){
+        robot.clickOn("#textBoxFieldName");
+        robot.write("@Field");
+        robot.clickOn("#addFieldButton");
+        TextField textBoxFieldName = (TextField) testScene.lookup("#textBoxFieldName");
+
+        Assertions.assertThat(textBoxFieldName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
+
+        robot.clickOn("#textBoxFieldName");
+        textBoxFieldName.clear();
+        robot.write("Field");
+        robot.clickOn("#addFieldButton");
+
+        Assertions.assertThat(textBoxFieldName.getStyle()).isEqualTo("-fx-text-box-border: #9A9A9A; -fx-focus-color: darkslateblue;");
+        Assertions.assertThat(textBoxFieldName.getText()).isEqualTo("");
+    }
+
+    @Test
+    void testAddBadMethodName(FxRobot robot) {
+        robot.clickOn("#textBoxMethodName");
+        robot.write("@Method");
+        robot.clickOn("#addMethodButton");
+        TextField textBoxClassName = (TextField) testScene.lookup("#textBoxMethodName");
+
+        Assertions.assertThat(textBoxClassName.getStyle()).isEqualTo("-fx-text-box-border: red ; -fx-focus-color: red ;");
     }
 
     @Test
@@ -82,7 +160,8 @@ public class ClassScreenViewTest {
         Assertions.assertThat(field.getName()).isEqualTo("Field");
         Assertions.assertThat(field.getType()).isEqualTo("Type");
 
-        // Assertions.assertThat(robot.lookup("#fieldDisplay").queryAs(ListView.class)).hasListCell(field);
+        // Assertions.assertThat(robot.lookup("#fieldDisplay").queryAs(Lis
+        // tView.class)).hasListCell(field);
     }
 
     @Test
